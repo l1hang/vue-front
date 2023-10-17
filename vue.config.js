0181a53587daf71d6a562e023b95748ee675d21b
l1hang <qaz1995xyz@163.com>
2023-10-17 11:49:13 +0800

    push

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  //配置跨域
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      }
-    }
-  }
+  transpileDependencies: true
 })
